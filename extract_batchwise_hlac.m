06876441b388fcd9cf31c6b81c1dd7d8050e4465
Atsushi Saitoh <lynxeyed@live.jp>
2022-10-12 00:58:14 +0900

    Create extract_batchwise_hlac.m

---
+++ extract_batchwise_hlac.m
@@ -0,0 +1,10 @@
+function hlac_batches = extract_batchwise_hlac(img, hlac_filters, nx, ny)
+%EXTRACT_BATCHWISE_HLAC この関数の概要をここに記述
+%   詳細説明をここに記述
+    batches = split_into_baches(img, nx, ny);
+
+    hlac_batches = [];
+
+
+end
+
