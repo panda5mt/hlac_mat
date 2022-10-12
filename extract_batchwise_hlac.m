287a4f39e1e62b45dc80f4edbf39c6f4cb5efe80
Atsushi Saitoh <lynxeyed@live.jp>
2022-10-12 16:34:22 +0900

    Update extract_batchwise_hlac.m

--- extract_batchwise_hlac.m
+++ extract_batchwise_hlac.m
@@ -1,6 +1,21 @@
 function hlac_batches = extract_batchwise_hlac(img, hlac_filters, nx, ny)
-%EXTRACT_BATCHWISE_HLAC この関数の概要をここに記述
-%   詳細説明をここに記述
+%EXTRACT_HLAC 2値化HLAC展開用にマスク(フィルタ)と画像データの畳み込みを行う
+%
+% hlac_batches = extract_hlac(img,hlac_filters,nx,ny)
+%     
+% Parameters
+% ----------
+% i㎎ : vector
+%   画像データ
+% hlac_filters : cell
+%   フィルタである数値配列(3次元と仮定) 
+% nx,ny : numeric
+%   imgを縦にny分割、横にnx分割する
+% 
+% Returns
+% -------
+% hlac_batches : vector(numeric)
+%   マスク(フィルタ)との一致回数
     batches = split_into_baches((img), nx, ny);
     b_size = size(batches,3); % バッチ総数
 
