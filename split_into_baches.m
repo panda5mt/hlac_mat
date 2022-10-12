d00005479a7c202ae1bdca640eeddb3211e2e07a
Atsushi Saitoh <lynxeyed@live.jp>
2022-10-12 15:44:13 +0900

    Update split_into_baches.m

--- split_into_baches.m
+++ split_into_baches.m
@@ -15,21 +15,43 @@ function batches = split_into_baches(img, nx, ny)
 % batches : vector(numeric)
 %   分割後の画像配列
     
-    batches = [];
     
     % 画像サイズ
-    x_size = size(img,2);
-    y_size = size(img,1);
+    x_size = uint16(size(img,2));
+    y_size = uint16(size(img,1));
     
     % 分割後の1つ分のサイズ
-    each_x = x_size / nx;
-    each_y = y_size / ny;
+    each_x = uint16(x_size / nx) - 1;
+    each_y = uint16(y_size / ny) - 1;
+    
+    % 画像サイズ再定義(半端な部分は切り捨てる) 
+    x_size = each_x * nx;
+    y_size = each_y * ny;
+    
 
+
+    % バッチの領域を確保(3次元)
+    batches = zeros(each_y,each_x,(nx * ny));
+
+    % 分割した画像をひたすら格納
+    i = 1;
     for y=1:each_y:y_size
         for x=1:each_x:x_size
-            batches = [batches img(y:(y + each_y - 1), x:(x + each_x - 1))]; 
+            
+            x_end = (x + each_x - 1);
+            y_end = (y + each_y - 1);
+            
+            %if x_end > x_size && y_end > y_size % 中途半端なところで終わる場合はやめる 
+
+                c_img = img(y:y_end, x:x_end); 
+                
+
+                batches(:,:,i) = c_img;
+                i = i + 1;
+            %end
         end
     end
+
 
 end
 
